b43de19da6f1bcce6aec2207cdab65d8d51534bb
earnerbaymalay <earnerbaymalay@gmail.com>
2026-04-11 03:14:25 +0800

    chore(gradle): migrate to Kotlin 2.0 Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -39,10 +40,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 
     packaging {
