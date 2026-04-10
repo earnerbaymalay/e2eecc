8f01c45f9736f26ced20dccd9cd11e6ca647c2a5
earnerbaymalay <earnerbaymalay@gmail.com>
2026-04-11 02:59:16 +0800

    chore(gradle): build fixes and version updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
-rootProject.name = "Cypherchat"
+rootProject.name = "Cyph3rChat"
 
 include(":app")
 include(":core:common")
